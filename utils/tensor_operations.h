f15ee646b11b7a1bff1f2627d915359ef8790dc0
Sahil Adhawade <sahil.adhawade@gmail.com>
2024-06-29 17:12:32 -0700

    naive linear (matmul foward & backward) implemented

--- utils/tensor_operations.h
+++ utils/tensor_operations.h
@@ -20,4 +20,43 @@ inline void threeDimWrite(float* x, int b, int t, int T, int C, float* val) {
     }
 }
 
+inline void naive_matmul_forward(float* X, float* Y, 
+                                 float* W, float* bias, 
+                                 int B, int T, int C, int OC) {
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+            int bt = b * T + t; 
+            for (int o = 0; o < OC; o++) {
+                int val = (bias != NULL) * bias[o];
+                for (int c = 0; c < C; c++) {
+                    val += twoDimRead(X, bt, c, C) * twoDimRead(W, o, c, C);
+                }
+                twoDimWrite(Y, bt, o, OC, val);
+            }
+        }
+    }
+}
+
+inline void naive_matmul_backward(float* X, float* W, 
+                                  float* DX, float* DY, float* DW, float* Dbias, 
+                                  int B, int T, int C, int OC) {
+    for (int b = 0; b < B; b++) {
+        for (int t = 0; t < T; t++) {
+            float* Dy = threeDimRead(DY, b, t, T, OC);
+            float* Dx = threeDimRead(DX, b, t, T, C);
+            float* x = threeDimRead(X, b, t, T, C);
+            for (int o = 0; o < OC; o++) {
+                float* Dw = twoDimRead(DW, o, 0, C);  
+                float* w = twoDimRead(W, o, 0, C);
+                float dy_o = Dy[o]; 
+                Dbias[o] += (Dbias != NULL) * dy_o;
+                for (int c = 0; c < C; c++) {
+                    Dx[c] += w[c] * dy_o;
+                    Dw[c] += x[c] * dy_o; 
+                }
+            }
+        }
+    }
+}
+
 #endif 
